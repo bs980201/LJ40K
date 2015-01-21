20a79574fa75561c8a10197473c017e2d407a4b9
bs980201 <bs980201@gmail.com>
2015-01-21 17:15:12 +0800

    rgba_gist+rgba_phog fusing

--- batch/batchSimple_fuse.py
+++ batch/batchSimple_fuse.py
@@ -8,7 +8,8 @@ from feelit.features import Fusion
 def help():
     print "usage: python [path_one][path_two][save_name]"
     print
-    print "  e.g: python '../exp/data/from_mongo/TFIDF.Xy.train.npz' '../exp/data/from_mongo/keyword.Xy.train.npz' 'TFIDF+keyword_fromMongo.Xy.train'"
+    print "  e.g: python '../exp/data/from_mongo/TFIDF.Xy.train.npz' '../exp/data/from_mongo/keyword.Xy.train.npz' 'from_mongo/TFIDF+keyword_fromMongo.Xy.train'"
+    print "  e.g: python '../exp/data/from_file/image_rgba_gist.Xy.npz' '../exp/data/from_file/image_rgba_phog.Xy.npz' 'from_file/rgba_gist+rgba_phog_fromfile.Xy.train'"
     exit(-1)
 
 if __name__ == '__main__':
